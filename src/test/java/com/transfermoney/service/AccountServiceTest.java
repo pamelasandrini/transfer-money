33b4de46b217217c76daba0a77cc2010a140dcc2
PAMELA SANDRINI BORSONI <pborsoni@br.ibm.com>
2019-01-31 15:57:14 -0200

    add unit test for account service

--- src/test/java/com/transfermoney/service/AccountServiceTest.java
+++ src/test/java/com/transfermoney/service/AccountServiceTest.java
@@ -1,25 +1,36 @@
 package com.transfermoney.service;
 
-import java.net.URI;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.util.EntityUtils;
-import org.junit.Assert;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.transfermoney.bo.Account;
+import com.transfermoney.dao.AccountDAO;
+import com.transfermoney.dao.AccountDAOImpl;
 import com.transfermoney.dao.ConnectionFactory;
-import com.transfermoney.util.PropertiesUtil;
 
-public class AccountServiceTest {
+public class AccountServiceTest extends JerseyTest {
 
-	private static final String ACCOUNT_SERVICE_URL = PropertiesUtil.getProperty("account_service_url");
-	private static final Client client = ClientBuilder.newClient();
+	private AccountDAO dao = new AccountDAOImpl();
+
+	@Override
+	protected Application configure() {
+		return new ResourceConfig(AccountService.class);
+	}
 
 	@Before
 	public void loadTestData() {
@@ -28,17 +39,100 @@ public class AccountServiceTest {
 	}
 
 	@Test
-	public void getAllAccountsTest() {
+	public void getAllAccountsEmptyListTest() {
 
-        URI uri = builder.setPath("/account/all").build();
-        HttpGet request = new HttpGet(uri);
-        HttpResponse response = client.execute(request);
-        int statusCode = response.getStatusLine().getStatusCode();
-        Assert.assertTrue(statusCode == 200);
-        //check the content
-        String jsonString = EntityUtils.toString(response.getEntity());
-        Account[] accounts = mapper.readValue(jsonString, Account[].class);
-        Assert.assertTrue(accounts.length > 0);
+		dao.deleteAccount(0);
+		Response response = target("/account/all").request().get();
+
+		assertEquals("Http Response should be 204: ", Status.NO_CONTENT.getStatusCode(), response.getStatus());
 	}
 
+	@Test
+	public void getAllAccountsTest() {
+		Response response = target("/account/all").request().get();
+
+		assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("Http Content-Type should be: ", MediaType.APPLICATION_JSON,
+				response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+
+		List<Account> content = response.readEntity(new GenericType<List<Account>>() {
+		});
+		assertTrue(content.size() > 0);
+	}
+
+	@Test
+	public void getAccountsTest() {
+		Response response = target("/account/0").request().get();
+
+		assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("Http Content-Type should be: ", MediaType.APPLICATION_JSON,
+				response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+
+		Account content = response.readEntity(Account.class);
+		assertEquals(0, content.getAccountNo());
+	}
+
+	@Test
+	public void getInexistingAccountsTest() {
+		Response response = target("/account/10").request().get();
+
+		assertEquals("Http Response should be 400: ", Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+		assertEquals("Http Content-Type should be: ", null, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+
+	}
+
+	@Test
+	public void createAccountsTest() {
+
+		Account account = new Account("test1", 250);
+
+		Response response = target("/account/create").request().put(Entity.entity(account, MediaType.APPLICATION_JSON));
+
+		assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("Http Content-Type should be: ", MediaType.APPLICATION_JSON,
+				response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+
+		Long content = response.readEntity(long.class);
+
+		assertEquals(1, content.longValue());
+	}
+
+	@Test
+	public void getBalanceTest() {
+		Response response = target("/account/0/balance").request().get();
+
+		assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+		assertEquals("Http Content-Type should be: ", MediaType.APPLICATION_JSON,
+				response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+
+		Double content = response.readEntity(double.class);
+		assertTrue(content == 1500);
+	}
+
+	@Test
+	public void getBalanceInexistingAccountTest() {
+		Response response = target("/account/10/balance").request().get();
+
+		assertEquals("Http Response should be 400: ", Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+		assertEquals("Http Content-Type should be: ", null, response.getHeaderString(HttpHeaders.CONTENT_TYPE));
+
+	}
+
+	@Test
+	public void deleteAccountTest() {
+
+		Response response = target("/account/0").request().delete();
+
+		assertEquals("Http Response should be 200: ", Status.OK.getStatusCode(), response.getStatus());
+
+	}
+
+	@Test
+	public void deleteInexistingAccountTest() {
+
+		Response response = target("/account/10").request().delete();
+
+		assertEquals("Http Response should be 400: ", Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+
+	}
 }
